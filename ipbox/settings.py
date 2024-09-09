3539c82a7868858e74a3749fbd67a33e27e26f59
Dominik Baran <dominik.baran7@gmail.com>
2024-09-09 15:38:51 +0200

    Update settings.py

--- ipbox/settings.py
+++ ipbox/settings.py
@@ -1,11 +1,11 @@
 azure_pat = ""
-git_authors = [x.lower() for x in ["dominik.baran@guestline.com", "domino.baran@gmail.com", "dominik.baran7@gmail.com", "Dominik Baran"]]
-author = 'dominik.baran@guestline.com'
+git_authors = [x.lower() for x in ["dominik.baran@XXX", "Dominik Baran"]]
+author = 'dominik.baran@XXX'
 excel_path="Ewidencja_projektowa_2023.xlsx"
 year = 2023
 from_month = 1
 to_month = 12
 
 heuristics_pr_filter_enabled = True # True speeds up script but might ommit PR with your commits but created by someone else
-projects = ["Rezlynx", "Search", "Sugoi", "Mandalore"]
+projects = ["TTT"]
 org_url = 'https://dev.azure.com/guestlinelabs'
